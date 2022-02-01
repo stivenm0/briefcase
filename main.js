5afecaf3564b3fea58b2f85c5e680edfc72875d0
stivenm0 <estivenyeizon@gmail.com>
2022-02-01 09:15:32 -0500

    one version

---
+++ main.js
@@ -0,0 +1,8 @@
+// let burger= document.querySelector('#burger');
+
+// function action(x){
+//     burger.classList.toggle('activo')
+//     x.classList.toggle('activo')
+//     ('.menu').classList.toggle('activo')
+// }
+
